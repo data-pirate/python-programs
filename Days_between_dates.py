a59acbf87dc6b6aec5683781d505b04fac72d829
Damanpreet Singh <43677641+data-pirate@users.noreply.github.com>
2020-05-16 20:17:39 +0530

    Create Days_between_dates.py

---
+++ Days_between_dates.py
@@ -0,0 +1,71 @@
+# PROGRAMMER: Damanpreet singh
+# created on: May 16, 2020
+
+
+def leapyear(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
+
+def days_of_month(year, month):
+    """Returns days of month"""
+    x = (1, 3, 5, 7, 8, 10, 12)
+    if month in x:
+        return 31
+    if month == 2:
+        if leapyear(year):
+            return 29
+        return 28
+    return 30
+
+def nextDay(year, month, day):
+    """Returns the next day of the passed month and year"""
+    if day < days_of_month(year, month):
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+        
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before year2-month2-day2. Otherwise, returns False."""
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False        
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2."""
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
+
+
+# Test cases to test code
+def test():
+    test_cases = [((2012,1,1,2012,2,28), 58), 
+                  ((2012,1,1,2012,3,1), 60),
+                  ((2011,6,30,2012,6,30), 366),
+                  ((2011,1,1,2012,8,8), 585 ),
+                  ((1900,1,1,1999,12,31), 36523)]
+    
+    for (args, answer) in test_cases:
+        result = daysBetweenDates(*args)
+        if result != answer:
+            print "Test with data:", args, "failed"
+        else:
+            print "Test case passed!"
+
+test()
